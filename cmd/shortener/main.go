235a099239192cbfc5bc98c78c43d13337210f02
Олег <oleg.phone@mail.ru>
2023-07-02 12:38:54 +0300

    А вдруг?

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
 	"encoding/json"
 	"io"
 	"net/http"
 	"reflect"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -60,9 +62,9 @@ func closeServer() {
 func mainRouter() chi.Router {
 	r := chi.NewRouter()
 
-	r.Handle(`/`, withLogging(setLink()))
-	r.Handle("/{shlink}", withLogging(getLink()))
-	r.Handle("/api/shorten", withLogging(setJSONLink()))
+	r.Handle(`/`, gzipHandle(withLogging(setLink())))
+	r.Handle("/{shlink}", gzipHandle(withLogging(getLink())))
+	r.Handle("/api/shorten", gzipHandle(withLogging(setJSONLink())))
 	//r.Get("/{shlink}", getLink())
 	//r.Post("/", setLink)
 	return r
@@ -272,4 +274,51 @@ func IsNil(obj interface{}) bool {
 	}
 
 	return false
+}
+
+func gzipHandle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// переменная reader будет равна r.Body или *gzip.Reader
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			gzp, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			r.Body = gzp
+			defer gzp.Close()
+		}
+		// проверяем, что клиент поддерживает gzip-сжатие
+		// это упрощённый пример. В реальном приложении следует проверять все
+		// значения r.Header.Values("Accept-Encoding") и разбирать строку
+		// на составные части, чтобы избежать неожиданных результатов
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			// если gzip не поддерживается, передаём управление
+			// дальше без изменений
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// создаём gzip.Writer поверх текущего w
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		// передаём обработчику страницы переменную типа gzipWriter для вывода данных
+		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+	})
+}
+
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	// w.Writer будет отвечать за gzip-сжатие, поэтому пишем в него
+	return w.Writer.Write(b)
 }
