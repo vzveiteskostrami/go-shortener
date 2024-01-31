8ce591ee66e7f7972ffbbc45108615a64bf8260f
vzveiteskostrami <bezdrotova.misha@yandex.ru>
2024-01-31 11:51:54 +0300

    Iter17 2nd review try 2

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -10,22 +10,18 @@ func Test_getOwnerID(t *testing.T) {
 	tests := []struct {
 		name        string
 		tokenString string
-		want        int64
 		want1       bool
 	}{
-		{"Wrong", "aaaaaaaaaaaaaaaaaaaaaaa", -1, false},
-		{"Right", "aaaaaaaaaaaaaaaaaaaaaaa", 1, true},
+		{"Wrong", "aaaaaaaaaaaaaaaaaaaaaaa", false},
+		{"Right", "aaaaaaaaaaaaaaaaaaaaaaa", true},
 	}
-	tests[1].tokenString, tests[1].want, _ = tokenService.makeToken()
+	tests[1].tokenString, _, _ = tokenService.makeToken()
 	logging.LoggingInit()
 	tokenService.NewOWNERID = 0
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := getOwnerID(tt.tokenString)
-			if got != tt.want {
-				t.Errorf("getOwnerID() got = %v, want %v", got, tt.want)
-			}
-			if (got1 == nil) == tt.want1 {
+			_, got1 := getOwnerID(tt.tokenString)
+			if ((got1 != nil) && tt.want1) || ((got1 == nil) && !tt.want1) {
 				t.Errorf("getOwnerID() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
